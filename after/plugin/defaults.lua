a83a1486de49d164db139b8fc63740b638ec674b
Youssouf da Silva <youssouf.dasilva@fox-robotics.com>
2023-03-13 16:20:42 +0000

    Updated the old neotree remap to toggle instead of only reveal

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,4 +1,5 @@
 vim.opt.relativenumber = true
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+vim.keymap.set('n', '<leader>\\', ':Neotree toggle<cr>', {desc = 'Neotree Toggle (escaping\\) '})
+-- vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
