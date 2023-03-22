6766202a46ca7bb14da1e1871784c637c1826ca8
youssoufdasilva <youssoufdasilva@gmail.com>
2023-03-22 08:34:35 +0000

    changed neotree toggle to focus

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,5 +1,6 @@
 vim.opt.relativenumber = true
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-vim.keymap.set('n', '<leader>\\', ':Neotree toggle<cr>', {desc = 'Neotree Toggle (escaping\\) '})
+-- vim.keymap.set('n', '<leader>\\', ':Neotree toggle<cr>', {desc = 'Neotree Toggle (escaping\\) '})
+vim.keymap.set('n', '<leader>\\', ':Neotree focus<cr>', {desc = 'Neotree Focus (escaping\\) '})
 -- vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
