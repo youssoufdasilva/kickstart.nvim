07a1eb67e8edf19da2769c5c5fcfd9540c03e683
youssoufdasilva <youssoufdasilva@gmail.com>
2023-03-25 10:54:56 +0000

    added cursor line highlight

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,4 +1,5 @@
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 -- vim.keymap.set('n', '<leader>\\', ':Neotree toggle<cr>', {desc = 'Neotree Toggle (escaping\\) '})
