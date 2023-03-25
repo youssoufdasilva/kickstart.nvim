1fe1feeaea462bd5151a0bab9dc3cb3ebaca8e42
youssoufdasilva <youssoufdasilva@gmail.com>
2023-03-25 17:45:35 +0000

    added another tab remap

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -4,6 +4,9 @@ vim.opt.cursorline = true
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>\\', ':Neotree focus<cr>', {desc = 'Neotree Focus (escaping\\)'})
 vim.keymap.set('n', '<leader>tt', ':tabnext<cr>', {desc = '[T]ab Nex[t]'})
+vim.keymap.set('n', '<leader>tp', ':tabprevious<cr>', {desc = '[T]ab [P]revious'})
 
 vim.keymap.set('n', '}', '}zz')
 vim.keymap.set('n', '{', '{zz')
+
+vim.keymap.set('n', '<leader>=', 'ggVG=', {desc = 'Auto Indent All'})
