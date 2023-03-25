fcea9262f04200d4226c90335d319ca8e457b8d2
youssoufdasilva <youssoufdasilva@gmail.com>
2023-03-25 11:36:29 +0000

    remapped { & } to center after jumping

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -2,6 +2,8 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
--- vim.keymap.set('n', '<leader>\\', ':Neotree toggle<cr>', {desc = 'Neotree Toggle (escaping\\) '})
-vim.keymap.set('n', '<leader>\\', ':Neotree focus<cr>', {desc = 'Neotree Focus (escaping\\) '})
--- vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+vim.keymap.set('n', '<leader>\\', ':Neotree focus<cr>', {desc = 'Neotree Focus (escaping\\)'})
+vim.keymap.set('n', '<leader>tt', ':tabnext<cr>', {desc = '[T]ab Nex[t]'})
+
+vim.keymap.set('n', '}', '}zz')
+vim.keymap.set('n', '{', '{zz')
